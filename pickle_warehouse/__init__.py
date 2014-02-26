e9f478a6a30e91b8aa33a7e341dfd6b355255a91
Thomas Levine <_@thomaslevine.com>
2014-02-26 10:52:25 +0000

    get tests running, and stub the function

--- pickle_warehouse/__init__.py
+++ pickle_warehouse/__init__.py
@@ -1,10 +1,3 @@
 class Warehouse:
     def __init__(self, cachedir):
         self.cachedir = cachedir
-
-
-
-'''
-import types
-if not isinstance(arg, types.StringTypes):
-'''
--- pickle_warehouse/identifiers.py
+++ pickle_warehouse/identifiers.py
@@ -1,2 +1,14 @@
+import types
+
+from urllib.request import urlparse
+
 def parse(cachedir, index):
-    return index
+    if isinstance(index, types.StringTypes):
+        path = [index]
+    else:
+        path = list(itertools.chain(map(parse_partial, index)))
+
+    return [cachedir] + path
+
+def parse_partial(item):
+    return item
