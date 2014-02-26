de13c71e3a92fe0d4cbe56e5fc23d99e84092190
Thomas Levine <_@thomaslevine.com>
2014-02-26 12:35:03 +0000

    items

--- pickle_warehouse/warehouse.py
+++ pickle_warehouse/warehouse.py
@@ -37,10 +37,11 @@ class Warehouse:
             for filename in filenames:
                 yield self[os.path.relpath(os.path.join(dirpath, filename), self.cachedir)]
 
-    def keys(self):
+    def items(self):
         for dirpath, _, filenames in os.walk(self.cachedir):
             for filename in filenames:
-                yield os.path.relpath(os.path.join(dirpath, filename), self.cachedir)
+                index = os.path.relpath(os.path.join(dirpath, filename), self.cachedir)
+                yield index, self[os.path.relpath(os.path.join(dirpath, filename), self.cachedir)]
 
 def _reversed_directories(outer, inner):
     while outer != inner:
