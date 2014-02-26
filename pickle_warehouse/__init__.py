feb129e126ae85df14876c5b1862a0d514ce2e55
Thomas Levine <_@thomaslevine.com>
2014-02-26 10:39:47 +0000

    test cases

--- pickle_warehouse/__init__.py
+++ pickle_warehouse/__init__.py
@@ -1,3 +1,10 @@
 class Warehouse:
     def __init__(self, cachedir):
         self.cachedir = cachedir
+
+
+
+'''
+import types
+if not isinstance(arg, types.StringTypes):
+'''
--- tests/test_warehouse.py
+++ tests/test_warehouse.py
@@ -10,11 +10,26 @@ class TestWarehouse(unittest.TestCase):
         self.tmp = tempfile.mkdtemp()
         self.w = Warehouse(self.tmp)
 
-    def test_set_tuple(self):
-        self.w[('a','b','c')] = 'abc'
+    def check_parse(self, index:str, path:list):
+        observed = self.parse(index)
+        n.assert_list_equal(observed, expected)
 
-    def test_set_list(self):
-        self.w[('a','b','c')] = 'abc'
+    def test_parse_tuple(self):
+        self.check_parse(('a','b','c'), [self.tmp, 'a', 'b', 'c'])
 
-    def test_set_list(self):
-        self.w[('a','b','c')] = 'abc'
+    def test_parse_list(self):
+        self.check_parse(['foo','bar','baz'], [self.tmp, 'foo', 'bar', 'baz'])
+
+    def test_parse_list(self):
+        self.check_parse('def', [self.tmp, 'def'])
+
+    def test_parse_slashes(self):
+        self.check_parse('left/middle/right', [self.tmp, 'left', 'middle', 'right'])
+
+    def test_parse_url(self):
+        self.check_parse('http://thomaslevine.com/!?foo=bar', [self.tmp, 'http', 'thomaslevine.com', '!?foo=bar'])
+
+    def test_parse_combination(self):
+        index = ['http://thomaslevine.com', 'foo/bar/baz', 'a b']
+        path = ['http', 'thomaslevine.com', 'foo', 'bar', 'baz', 'a b']
+        self.check_parse(index, path)
