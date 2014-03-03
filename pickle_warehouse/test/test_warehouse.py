b6a4c25bb4175339bfe6b7e28fa773002903f616
Thomas Levine <_@thomaslevine.com>
2014-03-03 21:51:22 +0000

    fix test

--- pickle_warehouse/test/test_warehouse.py
+++ pickle_warehouse/test/test_warehouse.py
@@ -117,8 +117,10 @@ class TestWarehouse(unittest.TestCase):
 
 
 def test_mkdir():
-    w = Warehouse('/tmp/not a directory')
-    rmtree('/tmp/not a directory')
+    d = '/tmp/not a directory'
+    w = Warehouse(d)
+    if os.path.exists(d):
+        rmtree(d)
     w[('abc','def','ghi')] = 3
     with open(os.path.join('/tmp/not a directory/abc/def/ghi'), 'rb') as fp:
         observed = pickle.load(fp)
